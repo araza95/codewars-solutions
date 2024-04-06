8375f0b5f28c004ef05568f218d4cad9de1fd40d
araza95 <alihaiderizvi.you@gmail.com>
2024-04-06 05:55:00 +0500

    `Update :` - Folder structure updated.

--- src/8Kyu/P1-rock-paper-sicissor/solution.ts
+++ src/8Kyu/P1-rock-paper-sicissor/solution.ts
@@ -1,3 +1,17 @@
+/*
+Rock Paper Scissors Problem
+
+Let's play! You have to return which player won! In case of a draw return Draw!.
+
+Examples(Input1, Input2 --> Output):
+
+```
+"scissors", "paper" --> "Player 1 won!"
+"scissors", "rock" --> "Player 2 won!"
+"paper", "paper" --> "Draw!"
+```
+*/
+
 function rps(p1: string, p2: string): string {
   const winsAgainst: { [key: string]: string } = {
     rock: "scissors",
