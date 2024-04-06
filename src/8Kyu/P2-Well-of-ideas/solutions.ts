6bb36b7c6613fa1fde38c55ec7fc2dbf2e55fdcc
araza95 <alihaiderizvi.you@gmail.com>
2024-04-06 06:17:51 +0500

    `(8Kyu) Problem 2:` - Well of ideas problem solved

---
+++ src/8Kyu/P2-Well-of-ideas/solutions.ts
@@ -0,0 +1,20 @@
+/*
+For every good kata idea there seem to be quite a few bad ones!
+
+In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+*/
+
+export function well(x: string[]): string {
+  const goodIdeasCount = x.reduce(
+    (count, idea) => (idea === "good" ? count + 1 : count),
+    0
+  );
+  return goodIdeasCount === 0
+    ? "Fail!"
+    : goodIdeasCount <= 2
+    ? "Publish!"
+    : "I smell a series!";
+}
+
+console.log(well(["bad", "bad", "bad"]));
